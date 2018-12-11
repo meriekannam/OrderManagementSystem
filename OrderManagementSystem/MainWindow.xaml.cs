6c4bc6df08f840c50d5bf66b12bd5ba2f5bccf56
Merie Kannampuzha <merie.kannampuzha@gmail.com>
2018-12-11 14:54:57 +0200

    Changes from Order Management System

--- OrderManagementSystem/MainWindow.xaml.cs
+++ OrderManagementSystem/MainWindow.xaml.cs
@@ -16,13 +16,50 @@ using System.Windows.Shapes;
 namespace OrderManagementSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xamlb
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DateTime tänään = DateTime.Today;
         public MainWindow()
         {
             InitializeComponent();
+            dpTilausPvm.SelectedDate = tänään;
+            dpToimitusPvm.SelectedDate = tänään.AddDays(14);
         }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TilausOtsikko TilausInstance = new TilausOtsikko();
+                TilausInstance.TilausNumero = int.Parse(txtTilausNumero.Text);
+                TilausInstance.TilausPvm = dpTilausPvm.SelectedDate.Value;
+                TilausInstance.AsiakkaanNimi = txtAsiakasNimi.Text;
+                TilausInstance.AsiakasNumero = int.Parse(txtAsiakasNimi.Text);
+                TilausInstance.ToimitusOsoite = txtOsoite.Text;
+                TilausInstance.ToimitusPvm = dpToimitusPvm.SelectedDate.Value;
+                //txtToimitusAika.Text = TilausInstance.LaskeToimitusAika();
+
+
+
+                MessageBox.Show("Tilaus tallennettu: Tialusnumero : " + "\r\n" + "Tialusnumero: " + TilausInstance.TilausNumero.ToString()
+                + "\r\n" + "Tilauspäivämäärä: " + TilausInstance.TilausPvm.ToString()
+                + "\r\n" + "Toimituspäivämäärä: " + TilausInstance.ToimitusPvm.ToString()
+                + "\r\n" + "AsiakkaanNimi: " + TilausInstance.AsiakkaanNimi
+                + "\r\n" + "AsiakasNumero: " + TilausInstance.AsiakasNumero.ToString()
+                + "\r\n" + "ToimitusOsoite: " + TilausInstance.ToimitusOsoite
+                
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+
     }
 }
+
---
+++ OrderManagementSystem/TilausOtsikko.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem
+{
+    class TilausOtsikko
+    {
+            private DateTime tilausPvm;
+            private DateTime toimitusPvm;
+            public int TilausNumero { get; set; }
+            public string AsiakkaanNimi { get; set; }
+            public int AsiakasNumero { get; set; }
+            public string ToimitusOsoite { get; set; }
+            public DateTime TilausPvm
+            {
+                get
+                {
+                    return tilausPvm;
+                }
+                set
+                {
+                    if (value <= DateTime.Today) //Dates can be checked as if it is a number
+                    {
+                        tilausPvm = value;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Tilauspvm ei voi olla tulevaisuudessa");
+                    }
+                }
+            }
+            public DateTime ToimitusPvm
+            {
+                get
+                {
+                    return toimitusPvm;
+                }
+                set
+                {
+                    if (value > DateTime.Today) //Dates can be checked as if it is a number
+                    {
+                        toimitusPvm = value;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("ToimitusPvm ei voi olla menniesyydessa");
+                    }
+                }
+            }
+
+            internal string LaskeToimitusAika()
+            {
+                TimeSpan erotus;
+                erotus = ToimitusPvm.Subtract(tilausPvm);
+                return erotus.ToString();
+            }
+
+    }
+}
+
