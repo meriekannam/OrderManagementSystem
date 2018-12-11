1bb9efa78a576581760a69738c9111399c2d718e
Merie Kannampuzha <merie.kannampuzha@gmail.com>
2018-12-11 15:24:16 +0200

    Code for total sum added

--- OrderManagementSystem/MainWindow.xaml.cs
+++ OrderManagementSystem/MainWindow.xaml.cs
@@ -26,6 +26,27 @@ namespace OrderManagementSystem
             InitializeComponent();
             dpTilausPvm.SelectedDate = tänään;
             dpToimitusPvm.SelectedDate = tänään.AddDays(14);
+            DataGridTextColumn textColumn1 = new DataGridTextColumn();
+            textColumn1.Binding = new Binding("TilausNumero");
+            DataGridTextColumn textColumn2 = new DataGridTextColumn();
+            textColumn2.Binding = new Binding("TuoteNumero");
+            DataGridTextColumn textColumn3 = new DataGridTextColumn();
+            textColumn3.Binding = new Binding("TuoteNimi");
+            DataGridTextColumn textColumn4 = new DataGridTextColumn();
+            textColumn4.Binding = new Binding("Maara");
+            DataGridTextColumn textColumn5 = new DataGridTextColumn();
+            textColumn5.Binding = new Binding("Hinta");
+            //DataGridin otsikot + edellä "ilmaan" luotujen sarakkeiden sijoitus konkreettiseen DataGridiin, joka on luotu formille
+            textColumn1.Header = "Tilauksen numero";
+            dgTilausRivit.Columns.Add(textColumn1);
+            textColumn2.Header = "Tuotenumero";
+            dgTilausRivit.Columns.Add(textColumn2);
+            textColumn3.Header = "Tuotenimi";
+            dgTilausRivit.Columns.Add(textColumn3);
+            textColumn4.Header = "Määrä";
+            dgTilausRivit.Columns.Add(textColumn4);
+            textColumn5.Header = "A-Hinta";
+            dgTilausRivit.Columns.Add(textColumn5);
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
@@ -57,9 +78,38 @@ namespace OrderManagementSystem
                 MessageBox.Show(ex.Message);
 
             }
+            txtTuoteNumero.Clear();
+            txtTuoteNimi.Clear();
+            txtMaara.Clear();
+            txtAHinta.Clear();
+
         }
 
+        private void BtnLisääRivi_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TilausRivi TilausR = new TilausRivi();
+                TilausR.TilausNumero = int.Parse(txtTilausNumero.Text);
+                TilausR.TuoteNumero = txtTuoteNumero.Text;
+                TilausR.TuoteNimi = txtTuoteNimi.Text;
+                TilausR.Maara = int.Parse(txtMaara.Text);
+                TilausR.AHinta = int.Parse(txtAHinta.Text);
 
+                MessageBox.Show("Tilausrivi tallennettu: Tialusnumero : " + "\r\n" + "Tialusnumero: " + TilausR.TilausNumero.ToString()
+                   + "\r\n" + "TuoteNumero: " + TilausR.TilausNumero.ToString()
+                   + "\r\n" + "TuoteNimi: " + TilausR.TuoteNimi
+                   + "\r\n" + "Määrä: " + TilausR.Maara
+                   + "\r\n" + "Hinta: " + TilausR.AHinta.ToString()
+                   );
+                dgTilausRivit.Items.Add(TilausR);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
     }
 }
 
